d64c3370e076062fe22f60bae8fc4f9e7f0aed3e
Youssef Shawki <ya0721435@gmail.com>
2024-12-24 08:21:39 +0200

    positional embeddings and encoder stack of layers

--- Transformer/encoder_decoder.py
+++ Transformer/encoder_decoder.py
@@ -1,5 +1,28 @@
 from self_attention import * 
 
+
+# we have used learnable positional embeddings for this implementation
+class Embeddings(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.token_embeddings = nn.Embedding(config.vocab_size, config.hidden_size)
+        self.pos_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size)
+        self.layer_norm = nn.LayerNorm(config.hidden_size, eps=1e-12) # eps for numerical stability
+        self.dropout = nn.Dropout()
+
+    def forward(self, input_ids):
+        seq_lenght = input_ids.size(1)
+        position_ids = torch.arange(seq_lenght, dtype=torch.long).unsqueeze(0)
+
+        token_embedings = self.token_embeddings(input_ids)
+        pos_embeddings = self.pos_embeddings(position_ids)
+
+        embeddings = token_embedings + pos_embeddings
+        embeddings = self.layer_norm(embeddings)
+        embeddings = self.dropout(embeddings)
+        
+        return embeddings
+
 class FeedForward(nn.Module):
     def __init__(self, config):
         super().__init__()
@@ -30,4 +53,18 @@ class TransformerEncoderLayer(nn.Module):
         x = x + self.attention(hidden_state)
         x = x + self.feed_forward(self.layer_norm_2(x))
         return x 
+    
+class TransformerEncoder(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.embeddings = Embeddings(config)
+        self.layers_stack = nn.ModuleList([
+            TransformerEncoderLayer(config) for _ in range(config.num_hidden_layers)
+        ])  
+
+    def forward(self, x):
+        x = self.embeddings(x)
+        for layer in self.layers_stack:
+            x = layer(x)
+        return x
     
