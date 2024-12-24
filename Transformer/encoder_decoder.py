bcf6749c65356acc6f804708212f0f361524a07c
Youssef Shawki <ya0721435@gmail.com>
2024-12-24 08:30:42 +0200

    classification head for masked language modeling

--- Transformer/encoder_decoder.py
+++ Transformer/encoder_decoder.py
@@ -68,3 +68,19 @@ class TransformerEncoder(nn.Module):
             x = layer(x)
         return x
     
+
+class TransformerForSequenceClassification(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.encoder = TransformerEncoder(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size, config.num_labels)
+
+    def forward(self, x):
+        x = self.encoder(x) [:,0,:] # select the first hidden state, conventional but not restricted.
+        x = self.dropout(x)
+        x = self.classifier(x)
+        return x
+    
+
+
